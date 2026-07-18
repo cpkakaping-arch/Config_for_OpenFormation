887fc0e383c737b160e91ef26119423a034fe3f1
cpkakaping-arch <cp.kakaping@gmail.com>
2026-07-18 19:27:51 +0200

    version scalable

--- script/python_pro/ecart_type.py
+++ script/python_pro/ecart_type.py
@@ -10,28 +10,26 @@ def somme_exp(x, tab):
     return sum_tableau(b)
 
 def ecart_type(tab):
-
-
     x = moyenne(tab)
     n = somme_exp(x, tab)
     c = math.sqrt((1 / (e - 1)) * n)
     print (f"{c}")
     return c
-    tab = []
+
+tab = []
 
 print("Ce programme est utilisé pour calculer l'écart-type selon la physique.\n")
 print("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.\n")
 
 e = int(input("Combien de nombres voulez-vous calculer leur écart-type ? "))
 
-if e < 2:
-    print("Il faut au moins deux valeurs.")
-    return
+if e >= 2:
+	print("Entrez à tour de rôle les nombres de votre tableau.")
+	print("NB : pour les nombres à virgule, utilisez le point (ex : 12.5).")
 
-print("Entrez à tour de rôle les nombres de votre tableau.")
-print("NB : pour les nombres à virgule, utilisez le point (ex : 12.5).")
-
-for i in range(e):
-    a = float(input(f"N°{i+1} = "))
-    tab.append(a)
-ecart_type(tab)
+	for i in range(e):
+    		a = float(input(f"N°{i+1} = "))
+    		tab.append(a)
+	ecart_type(tab)
+else:
+	print ("Il faut au minimum deux valeurs pour executer ce programme")
