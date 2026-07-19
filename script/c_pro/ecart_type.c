88bfa0ebedff1778e9be3edc978618cf28235028
cpkakaping-arch <cp.kakaping@gmail.com>
2026-07-19 17:40:19 +0200

    yo

---
+++ script/c_pro/ecart_type.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+int e;
+printf("Ce programme est utilisé pour calculer l'écart-type selon la physique.");
+printf("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.");
+
+puts ("Combien de nombres voulez-vous calculer leur é")
+scanf ("%d",&e);
+if (e >= 2)
+{
+        printf("Entrez à tour de rôle les nombres de votre tab");
+        print("NB : pour les nombres à virgule, utilisez le p");
+}
+}
