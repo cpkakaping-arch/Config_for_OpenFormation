4fa4509798c7d66389c6a329d41c97436225c3bf
cdric <cedvict@gmail.com>
2026-07-21 20:44:58 +0200

    Salut

--- script/python_pro/ecart_type.py
+++ script/python_pro/ecart_type.py
@@ -1,4 +1,4 @@
-from fonction import *
+from fonction import moyenne,sum_tableau
 import math
 
 def somme_exp(x, tab):
@@ -9,7 +9,7 @@ def somme_exp(x, tab):
         b.append(a)
     return sum_tableau(b)
 
-def ecart_type(tab):
+def ecart_type(e,tab):
     x = moyenne(tab)
     n = somme_exp(x, tab)
     c = math.sqrt((1 / (e - 1)) * n)
@@ -18,18 +18,20 @@ def ecart_type(tab):
 
 tab = []
 
-print("Ce programme est utilisé pour calculer l'écart-type selon la physique.\n")
+print("Ce programme calcule l'écart-type expérimental.\n")
 print("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.\n")
 
 e = int(input("Combien de nombres voulez-vous calculer leur écart-type ? "))
-
+	
+	
 if e >= 2:
-	print("Entrez à tour de rôle les nombres de votre tableau.")
-	print("NB : pour les nombres à virgule, utilisez le point (ex : 12.5).")
+    print("Entrez à tour de rôle les nombres de votre tableau.\n")
+    print("NB : pour les nombres à virgule, utilisez le point (ex : 12.5).")
+    for i in range(e):
+        a = float(input(f"N°{i+1} = "))
+        tab.append(a)
 
-	for i in range(e):
-    		a = float(input(f"N°{i+1} = "))
-    		tab.append(a)
-	ecart_type(tab)
+    ecart_type(tab)
+
 else:
-	print ("Il faut au minimum deux valeurs pour executer ce programme")
+    print ("Il faut au minimum deux valeurs pour executer ce programme")
