b1665c70515abcf53b10ac490822f0815a34aa21
cdric <cedvict@gmail.com>
2026-07-21 20:52:04 +0200

    Salut

--- script/c_pro/ecart_type.c
+++ script/c_pro/ecart_type.c
@@ -2,15 +2,17 @@
 
 int main ()
 {
-int e;
-printf("Ce programme est utilisé pour calculer l'écart-type selon la physique.");
-printf("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.");
 
-puts ("Combien de nombres voulez-vous calculer leur é")
-scanf ("%d",&e);
-if (e >= 2)
-{
-        printf("Entrez à tour de rôle les nombres de votre tab");
-        print("NB : pour les nombres à virgule, utilisez le p");
-}
+        int e;
+        printf("Ce programme est utilisé pour calculer l'écart-type selon la physique.");
+        printf("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.");
+
+        puts ("Combien de nombres voulez-vous calculer leur écart-type ?");
+        scanf ("%d",&e);
+        if (e >= 2)
+        {
+                printf("Entrez à tour de rôle les nombres de votre tableau :");
+                printf("NB : pour les nombres à virgule, utilisez le point (.) comme séparateur.");
+        }
+
 }
