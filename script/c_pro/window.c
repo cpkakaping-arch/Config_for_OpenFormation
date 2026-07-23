00ce1ca0da9887f80353e591a48b89e1f09a298d
perfectcp-sys <cedricparfaitperfectcp.@gmail.com>
2026-07-23 12:39:04 +0200

    win

--- script/c_pro/window.c
+++ script/c_pro/window.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include "fonction.h"
 #include <stdlib.h>
+#include <windows.h>
 int main ()
 {
+        
         char *ps = getenv("PSModulePath");
 
     if (ps != NULL)
     {
-        printf("PowerShell détecté\n");
-        system("powershell -ExecutionPolicy Bypass -File ./start.ps1");
+        printf("PowerShell detecte\n");
+        //system("powershell -ExecutionPolicy Bypass -File ./start.ps1");
+        system("start.bat");
     }
     else
     {
-        printf("CMD détecté\n");
+        printf("CMD detecte\n");
         system("start.bat");
     }
         int e;
-        puts("Ce programme est utilisé pour calculer l'écart-type selon la physique.\nL'écart-type est une mesure de la dispersion des valeurs d'un échantillon par rapport à la moyenne de cet échantillon.\nIl est calculé en prenant la racine carrée de la variance, qui est la moyenne des carrés des écarts entre chaque valeur et la moyenne de l'échantillon.\n");
-        puts("Le calcul de l'écart-type ne concerne que les tableaux contenant au moins deux valeurs.\n");
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+        system("color 0F");
+        system("cls");
+        puts("Ce programme est utilise pour calculer l'ecart-type selon la physique.\nL'ecart-type est une mesure de la dispersion des valeurs d'un echantillon par rapport a la moyenne de cet echantillon.\nIl est calcule en prenant la racine carree de la variance, qui est la moyenne des carres des ecarts entre chaque valeur et la moyenne de l'echantillon.\n");
+        puts("Le calcul de l'ecart-type ne concerne que les tableaux contenant au moins deux valeurs.\n");
 
-        printf("Combien de nombres voulez-vous calculer leur écart-type ? : ");
+        printf("Combien de nombres voulez-vous calculer leur ecart-type ? : ");
         scanf ("%d",&e);
         if (e >= 2)
         {
 
-                puts("Entrez à tour de rôle les nombres de votre tableau :");
-                puts("NB : pour les nombres à virgule, utilisez le point (.) comme séparateur.");
+                puts("Entrez a tour de role les nombres de votre tableau :");
+                puts("NB : pour les nombres a virgule, utilisez le point (.) comme separateur.");
                 double tab[e];
                 for (int i = 0; i < e; i++)
                 {
@@ -33,6 +40,9 @@ int main ()
                         scanf("%lf", &tab[i]);
                 }
                 ecart_type(e, tab);
+                puts("Appuyer sur une touhe pour sortir\n\t\t\t By Dieson Parfait");
+                getchar();
+                system("pause");
                 return 0;
         }
         else
